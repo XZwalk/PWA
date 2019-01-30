6455acd27eb05263144a343b4ef7cfff480c01a5
zhangxiang <zhangxiang@myhexin.com>
2019-01-30 19:27:37 +0800

    尝试解决问题

--- ZXPWA/scripts/app.js
+++ ZXPWA/scripts/app.js
@@ -72,7 +72,8 @@
 
     if ('serviceWorker' in navigator) {
         navigator.serviceWorker
-            .register('../service-worker.js')
+            ///PWA/ZXPWA/api/download.json
+            .register('/PWA/ZXPWA/service-worker.js')
             .then(function () {
                 console.log('Service Worker Registered');
             })

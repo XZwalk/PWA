9de85e74e3d33ea296309bf241fe617fadf404c4
zhangxiang <zhangxiang@myhexin.com>
2019-03-14 17:46:07 +0800

    初步完成布局

--- GiftBookPWA/src/js/index/enter.js
+++ GiftBookPWA/src/js/index/enter.js
@@ -34,34 +34,125 @@ document.getElementById("changeKey").onclick = function () {
 // You may need to reload the page, or check that the service worker for the current page
 // also controls the start URL from the manifest
 if (navigator.serviceWorker != null) {
-    navigator.serviceWorker.register('/PWA/LoanPWA/sw.js')
+    navigator.serviceWorker.register('/PWA/GiftBookPWA/sw.js')
         .then(function (registration) {
             console.log('Registered events at scope: ', registration.scope);
         });
 }
 
-//表格
-function altRows(id){
-    if(document.getElementsByTagName){
-
-        var table = document.getElementById(id);
-        var rows = table.getElementsByTagName("tr");
-
-        for(let i = 0; i < rows.length; i++){
-            if(i % 2 == 0){
-                rows[i].className = "even-row-color";
-            }else{
-                rows[i].className = "odd-row-color";
-            }
-        }
+setTimeout(() => {
+    let allData = null;
+    if (window.zxData) {
+        allData = JSON.parse(window.zxData);
     }
+
+    if (!allData) return;
+
+    createAllTables(allData);
+
+}, 1000);
+
+
+function createAllTables(allData) {
+    let myTotalAmount = getMyTotalAmount(allData);
+    let myTable = getTable("我的", ["序号", "姓名", "金额", "备注"], allData.weddingData.myData, myTotalAmount);
+
+    let main = document.getElementById("myBody");
+    main.appendChild(myTable);
 }
 
-window.onload=function(){
-    //表格隔行展示UI
-    altRows('bus-total-div-table');
-};
 
+function getMyTotalAmount(allData) {
+    let weddingData = allData.weddingData ? allData.weddingData : {};
+    let handleAry = weddingData.myData ? weddingData.myData : [];
+    let totalAmount = 0;
+    for (let i = 0; i < handleAry.length; i++) {
+        let item = handleAry[i];
+        totalAmount = totalAmount + parseInt(item.amount);
+    }
+
+    return totalAmount;
+}
+
+
+
+function getTable(title, tableTitleAry, tableListAry, totalAmount) {
+    let div = document.createElement('div');
+
+    let h2 = document.createElement('h2');
+    h2.innerHTML = title;
+    div.appendChild(h2);
+
+    let table = document.createElement('table');
+    table.className = "alt-rows-table";
+    div.appendChild(table);
+
+    let tr = document.createElement('tr');
+    tr.innerHTML = "";
+    table.appendChild(tr);
+
+    //创建表头
+    for (let i = 0; i < tableTitleAry.length; i++) {
+        let tableTitle = tableTitleAry[i];
+        let th = document.createElement('th');
+        th.innerHTML = tableTitle;
+        tr.appendChild(th);
+    }
+
+
+    for (let i = 0; i < tableListAry.length; i++) {
+        let tableListDic = tableListAry[i];
+        let row = document.createElement('tr'); //创建行
+        table.appendChild(row);
+
+        if(i % 2 == 0){
+            row.className = "even-row-color";
+        }else{
+            row.className = "odd-row-color";
+        }
+
+        let indexCell = document.createElement('td');
+        indexCell.innerHTML = i + 1;
+        row.appendChild(indexCell);
+
+        let nameCell = document.createElement('td');
+        nameCell.innerHTML = tableListDic.name;
+        row.appendChild(nameCell);
+
+        let amountCell = document.createElement('td');
+        amountCell.innerHTML = tableListDic.amount;
+        row.appendChild(amountCell);
+
+        let markCell = document.createElement('td');
+
+        let markInfo = "";
+        if (parseInt(tableListDic.state) === 1) {
+            markInfo = "";
+        } else if (parseInt(tableListDic.state) === 2) {
+            markInfo = "已清";
+        }
+        markCell.innerHTML = markInfo;
+        row.appendChild(markCell);
+    }
+
+
+    let row = document.createElement('tr'); //创建行
+    table.appendChild(row);
+
+    row.className = "odd-row-color";
+
+    let nameCell = document.createElement('td');
+    nameCell.innerHTML = "总金额";
+    row.appendChild(nameCell);
+
+    let amountCell1 = document.createElement('td');
+    //不能将s设置成小写的,否则失效
+    amountCell1.colSpan = "3";
+    amountCell1.innerHTML = totalAmount;
+    row.appendChild(amountCell1);
+
+    return div;
+}
 
 
 //.html()用为读取和修改元素的HTML标签

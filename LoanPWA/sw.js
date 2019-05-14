dd71c64a5cf413fae82ba855af46ddaf123f2727
zhangxiang <zhangxiang@myhexin.com>
2019-05-14 19:09:29 +0800

    调整缓存策略

--- LoanPWA/sw.js
+++ LoanPWA/sw.js
@@ -1,54 +1,80 @@
 //当页面发生修改时, 要同时对 sw.js 文件进行一次修改。
 //保证文件发生改变, 同时缓存的名称也变改变了
-let PRECACHE = 'loan-pwa-1.4';
-let RUNTIME = 'runtime';
 
-// list the files you want cached by the service worker
-PRECACHE_URLS = [
-    // 'index.html',
+var cacheName = 'loan-pwa-1.5';
+var filesToCache = [
     '/PWA/LoanPWA/dist/',
-    // 'style.css',
-    // 'main.js'
 ];
 
-
-// the rest below handles the installing and caching
-self.addEventListener('install', event => {
-    event.waitUntil(
-        caches.open(PRECACHE).then(cache => cache.addAll(PRECACHE_URLS)).then(self.skipWaiting())
+self.addEventListener('install', function(e) {
+    e.waitUntil(
+        caches.open(cacheName).then(function(cache) {
+            return cache.addAll(filesToCache);
+        })
     );
 });
 
-self.addEventListener('activate', event => {
-    const currentCaches = [PRECACHE, RUNTIME];
-    event.waitUntil(
-        caches.keys().then(cacheNames => {
-            return cacheNames.filter(cacheName => !currentCaches.includes(cacheName));
-        }).then(cachesToDelete => {
-            return Promise.all(cachesToDelete.map(cacheToDelete => {
-                return caches.delete(cacheToDelete);
-            }));
-        }).then(() => self.clients.claim())
-    );
-});
-
-self.addEventListener('fetch', event => {
-    if (event.request.url.startsWith(self.location.origin)) {
-        event.respondWith(
-            caches.match(event.request).then(cachedResponse => {
-                if (cachedResponse) {
-                    return cachedResponse;
+self.addEventListener('activate', function(e) {
+    e.waitUntil(
+        caches.keys().then(function(keyList) {
+            return Promise.all(keyList.map(function(key) {
+                if (key !== cacheName) {
+                    return caches.delete(key);
                 }
+            }));
+        })
+    );
+});
 
-                return caches.open(RUNTIME).then(cache => {
-                    return fetch(event.request).then(response => {
-                        // Put a copy of the response in the runtime cache.
-                        return cache.put(event.request, response.clone()).then(() => {
+self.addEventListener('fetch', function(e) {
+    var extendDataUrl = [
+        '/PWA/ZXPWA/api/download.json'
+    ];
+
+    var allDataUrl = extendDataUrl;
+    var requestIsDataApi = false;
+
+    //如果是 API 请求，先网络后缓存
+    for (count in extendDataUrl){
+        if (e.request.url.indexOf(extendDataUrl[count]) > -1 ) {
+            requestIsDataApi = true;
+            e.respondWith(
+                fetch(e.request)
+                    .then(function(response) {
+                        return caches.open(cacheName).then(function(cache){
+                            cache.put(e.request.url, response.clone());
                             return response;
                         });
-                    });
-                });
-            })
-        );
+                    })
+                    .catch(function(){
+                        return caches.match(e.request.url);
+                    })
+            );
+            break;
+        }
     }
+
+    //一般资源请求，先缓存再网络再默认
+    if (!requestIsDataApi){
+        e.respondWith(
+            caches.match(e.request).then(function(respond){
+                return respond || fetch(e.request)
+                    .then(function(res){
+                        return caches.open(cacheName).then(function(cache){
+                            if (e.request.url.indexOf("xzwalk.github.io") != -1) {
+                                cache.put(e.request.url, res.clone());
+                            } else {
+                                console.log(e.request.url);
+                            }
+                            return res;
+                        });
+                    })
+                    .catch(function(){
+                        return caches.match('offline.html');
+                    });
+            })
+        )
+    }
+
 });
+
